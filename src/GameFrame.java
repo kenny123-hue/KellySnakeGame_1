cf409f90618a4299c0d294a4b0086568df5f8848
Peter ken munyoki <82097393+kenny123-hue@users.noreply.github.com>
2022-03-26 14:10:17 +0300

    Add files via upload

---
+++ src/GameFrame.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kelvinolang
+ */
+public class GameFrame extends JFrame {
+    
+    GameFrame(){
+        GamePanel panel =new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/SnakeGame.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kelvinolang
+ */
+public class SnakeGame {
+    public static void main(String[] args){
+        
+        GameFrame frame = new GameFrame();
+        
+    }
+    
+}
